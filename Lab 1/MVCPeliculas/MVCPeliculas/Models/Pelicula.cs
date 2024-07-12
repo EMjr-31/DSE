41268d3e23d8abe776853603c46dcaf5fd23917e
Eduardo Martinez Jr <adonay.chacon12@gmail.com>
2024-07-11 19:00:30 -0600

    Guia 2 hasta pagina 12

--- Lab 1/MVCPeliculas/MVCPeliculas/Models/Pelicula.cs
+++ Lab 1/MVCPeliculas/MVCPeliculas/Models/Pelicula.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,17 @@ namespace MVCPeliculas.Models
     public class Peliculas
     {
         public int ID { get; set; }
+        [StringLength(250)]
+        [Required]
         public string Titulo { get; set; }
         public DateTime FechaLanzmiento { get; set; }
+        [StringLength(15)]
+        [Required]
         public string Genero { get; set; }
         public decimal Precio { get; set; }
+        [StringLength(250)]
+        [Required]
+        public string Director { get; set; }
 
     }
 }
